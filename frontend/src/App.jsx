73a5847bfa6c92d7cf191d7539af5796235dc6b2
houssemchatibi <houssem.chatibi@gmail.com>
2024-09-06 19:23:15 +0100

    add dark/light button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
+
+import Navbar from "./components/Navbar"
+
 
 
 function App() {
-  
+
 
   return (
-    <>
-      
-      <p >
-        Hello
+    <div>
+      <Navbar />
+      <p className="text-3xl md:text-5xl text-center font-bold uppercase tracking-wide mb-8 
+      bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">
+        My friends
       </p>
-    </>
+
+    </div>
   )
 }
 
